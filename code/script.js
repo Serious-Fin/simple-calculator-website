c067f6c9bdde69fcc11ad32ff523f5bd0a590f55
Serious-Fin <augustasgri@gmail.com>
2023-01-20 17:21:59 +0200

    Create number parsing logic with buttonClicked(...)

--- code/script.js
+++ code/script.js
@@ -1,3 +1,8 @@
+let FIRST_NUMBER = null;
+let SECOND_NUMBER = null;
+let OPERATOR = null;
+
+
 function add(num1, num2) {
     return num1 + num2;
 }
@@ -29,14 +34,60 @@ function operate(num1, operator, num2) {
     }
 }
 
-function updateDisplay(symbol) {
-    if (typeof +symbol === 'number') {
+function buttonClicked(symbol) {
+    /*if (typeof +symbol === 'number') {
         const output = document.querySelector('p#outputText');
         output.textContent = symbol;
+        DISPLAY_VALUE = +symbol;
+        console.log(DISPLAY_VALUE);
+    }*/
+
+    const output = document.querySelector('p#outputText');
+
+    if (!isNaN(symbol)){
+        console.log(`number ${+symbol} typed`);
+        if (FIRST_NUMBER === null) {
+            console.log('printing first');
+            FIRST_NUMBER = symbol;
+            output.textContent = FIRST_NUMBER;
+        }
+        else if (FIRST_NUMBER !== null && OPERATOR === null) {
+            console.log('printing first');
+            FIRST_NUMBER += symbol;
+            output.textContent = FIRST_NUMBER;
+        }
+        else if (SECOND_NUMBER === null && OPERATOR !== null && FIRST_NUMBER !== null) {
+            console.log('printing second');
+            SECOND_NUMBER = symbol;
+            output.textContent = SECOND_NUMBER;
+        }
+        else if (SECOND_NUMBER !== null) {
+            console.log('printing second');
+            SECOND_NUMBER += symbol;
+            output.textContent = SECOND_NUMBER;
+        }
     }
+    else if (symbol === '+' || symbol === '-' || symbol === '*' || symbol === '/') {
+        if (FIRST_NUMBER !== null) {
+            console.log(`Operator ${symbol} typed`)
+            OPERATOR = symbol;
+            output.textContent = OPERATOR;
+        } 
+    }
+    else if (symbol === '=') {
+        let result = operate(+FIRST_NUMBER, OPERATOR, +SECOND_NUMBER);
+        output.textContent = result;
+
+        FIRST_NUMBER = result;
+        OPERATOR = null;
+        SECOND_NUMBER = null;
+    }
+
+    console.log(`first: ${FIRST_NUMBER}; second: ${SECOND_NUMBER}; operator: ${OPERATOR}`);
+
 }
 
-function buttonClicked(event) {
+function buttonClickedAnimation(event) {
     this.classList.add('pressedButton');
 }
 
@@ -49,7 +100,7 @@ function removeClass(event) {
 
 const buttonNodeArray = document.querySelectorAll("button.genericButton");
 buttonNodeArray.forEach(button => {
-    button.addEventListener('click', buttonClicked);
+    button.addEventListener('click', buttonClickedAnimation);
 });
 
 buttonNodeArray.forEach(button => {
